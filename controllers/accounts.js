ab391286dbd81f7fea9c442ec44766ad2a66a53d
rjtengco1988 <ronaldjoytengco@yahoo.com>
2023-02-19 12:32:55 +0800

    Done

--- controllers/accounts.js
+++ controllers/accounts.js
@@ -1,10 +1,12 @@
 const path = require('path');
 const dbConnect = require('../db.js');
+const {ObjectId} = require('mongodb');
+
 
 exports.accounts = (req,res) => {
-let db = dbConnect.getDb()
-let accountsArray = [];
 
+let accountsArray = [];
+let db = dbConnect.getDb()
 
 db.collection('accounts')
 .find()
@@ -17,5 +19,29 @@ db.collection('accounts')
     res.status(500).json({error:"could not fetch the documents"})
 })
 
+
+};
+
+exports.accountGetById = (req,res) => {
+
+    let db = dbConnect.getDb()
+
+    if(ObjectId.isValid(req.params.id)) {
+
+        db.collection('accounts')
+        .findOne({_id : new ObjectId(req.params.id)})
+        .then(doc=>{
+            res.status(200).json(doc);
+        })
+        .catch(() => {
+            res.status(500).json({error:"Could not fetch document"})
+        })
+
+
+    }else{
+        res.status(500).json({error:"Wrong ID Format"})
+    }
+
+    
 
 }
--- router/router.js
+++ router/router.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const {accounts} = require('../controllers/accounts');
+const {accounts,accountGetById} = require('../controllers/accounts');
 
 const router = express.Router();
 
 router.get('/accounts', accounts);
+router.get('/accountsprofile/:id', accountGetById);
 
 
 module.exports = router;
