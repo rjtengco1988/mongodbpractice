4004cec7154b1682a66a6b3ee524bfb5e53d53ec
rjtengco1988 <ronaldjoytengco@yahoo.com>
2023-02-19 11:12:30 +0800

    Connected to mongod db

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 const express = require("express");
 const router = require('./router/router.js');
+const {connectToDb,getDb} = require('./db.js');
+const dotenv = require("dotenv");
 const app = express();
+dotenv.config();
 
-const port = 3000;
-app.listen(port, () => {
-    console.log(`App is now listening on port ${port}`);
-});
+let db
+
+connectToDb((err) => {
+
+    if(!err){
+        const port = process.env.PORT || 8080;
+        app.listen(port, () => {console.log(`node js listening to port ${port}`)})
+        console.log("DB CONNECTED");
+    }
+
+    db = getDb();
+
+})
 
 app.use('/',router);
---
+++ db.js
@@ -0,0 +1,27 @@
+const { MongoClient } = require('mongodb');
+const dotenv = require("dotenv");
+dotenv.config();
+
+let dbConnection;
+
+
+
+module.exports = {
+
+connectToDb : (cb) => {
+    MongoClient.connect(process.env.MONGO_URI)
+    .then((client)=>{
+            dbConnection = client.db();
+            return cb();
+    })
+    .catch(err => {
+        console.log(err);
+        return cb();
+    })
+},
+
+getDb : () => dbConnection
+
+
+
+}
